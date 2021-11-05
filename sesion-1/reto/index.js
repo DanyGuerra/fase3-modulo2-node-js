054c86ae31dd0d36114a09978194a6de870b75c0
DanyGuerra <luis_daniel.grra15@hotmail.com>
2021-11-05 17:50:33 -0600

    Solucion del reto de la sesion 1

--- sesion-1/reto/index.js
+++ sesion-1/reto/index.js
@@ -2,8 +2,41 @@ const random = require("./Random");
 
 const array = [];
 
-for (let i = 0; i < 20; i++) {
-  array.push(random(-1000, 1000, 10));
+for (let i = 0; i < 10; i++) {
+  array.push(random(-10, 20, 0));
 }
 
-console.log(array.sort((a, b) => a - b));
+//************************************************Ordenar usando sort()
+// function ordenarArray(a, b) {
+//   return a - b;
+// }
+
+// array.sort(ordenarArray);
+
+//***********************************************Ordenar bubble sort (ordenamiento burbuja)
+
+const bubbleSort = (arr) => {
+  const l = arr.length;
+  for (let i = 0; i < l; i++) {
+    for (let j = 1; j < l - i; j++) {
+      if (arr[j - 1] > arr[j]) {
+        [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]];
+        debugger;
+      }
+    }
+  }
+
+  return arr;
+};
+
+//********************************Ordenamiento por canasta
+// const bucketSort = (list) => {
+//   let minValue = list[0];
+//   let maxValue = list[0];
+
+//   list.forEach((current) => {
+//     current < minValue ? (minValue = current) : (maxValue = current);
+//   });
+// };
+
+console.log(bubbleSort(array));
