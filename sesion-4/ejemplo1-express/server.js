2cd891fab1dfdf5f92c1f6e11c9e3c5fa226c234
DanyGuerra <luis_daniel.grra15@hotmail.com>
2021-11-22 17:45:14 -0600

    Agregando helmet al proyecto

--- sesion-4/ejemplo1-express/server.js
+++ sesion-4/ejemplo1-express/server.js
@@ -3,10 +3,14 @@ const express = require("express");
 const morgan = require("morgan");
 const PORT = process.env.PORT || 3000;
 const server = express();
+const helmet = require("helmet");
 
 // body parser
 server.use(express.json());
 // logger
+
+// uso de helmet
+server.use(helmet());
 
 server.use(
   morgan(":method :url :status :res[content-length] - :response-time ms")
@@ -106,111 +110,3 @@ server.use((req, res) => {
   res.send("Welcome");
 });
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
-// require("dotenv").config();
-// const express = require("express");
-// const morgan = require("morgan");
-// ​
-// const PORT = process.env.PORT || 3000;
-// const server = express();
-// ​
-// // body parser for JSON
-// server.use(express.json());
-// ​
-// // HTTP logger
-// server.use(
-//   morgan(":method :url :status :res[content-length] - :response-time ms")
-// );
-// ​
-// // headers logger
-// server.use((req, res, next) => {
-//   console.log("Request Headers", JSON.stringify(req.headers, null, 4));
-//   next();
-// });
-// ​
-// // Pet middleware
-// server.use((req, res, next) => {
-//   if (req.get("PeT-TyPe") === "Cat") {
-//     return res.send("Miau!");
-//   } else if (req.get("PET-TYPE") === "Dog") {
-//     return res.send("Woof!");
-//   }
-//   next();
-// });
-// ​
-// // GET /api/users
-// server.get("/api/users", (req, res) => {
-//   res.json({
-//     users: 20,
-//   });
-// });
-// ​
-// // POST /api/users
-// server.post("/api/users", (req, res) => {
-//   const body = req.body;
-//   console.log("body->", body);
-//   // create a user
-//   res.status(201);
-//   res.json({
-//     user: {
-//       id: Date.now(),
-//       ...body,
-//     },
-//   });
-// });
-// ​
-// // GET /api/users/:id
-// server.get("/api/users/:id", (req, res) => {
-//   const userId = req.params.id;
-//   res.json({
-//     user: {
-//       id: userId,
-//       name: "Isaac",
-//       email: "iramirez@example.com",
-//     },
-//   });
-// });
-// ​
-// // path param = :id
-// // query param = ?userId=1232
-// // PUT /api/users/:id
-// server.put("/api/users/:id", (req, res) => {
-//   const userId = req.params.id;
-//   const body = req.body;
-//   res.json({
-//     user: {
-//       id: userId,
-//       ...body,
-//     },
-//   });
-// });
-// ​
-// // PATCH /api/users/:id
-// server.patch("/api/users/:id", (req, res) => {
-//   const userId = req.params.id;
-//   const body = req.body;
-//   res.json({
-//     user: {
-//       id: userId,
-//       ...body,
-//     },
-//   });
-// });
-// ​
-// // DELETE /api/users/:id
-// ​
-// server.delete("/api/users/:id", (req, res) => {
-//   const userId = req.params.id;
-//   res.json({
-//     user: {
-//       id: userId,
-//     },
-//   });
-// });
-// ​
-// // 404 handler
-// server.use((req, res) => {
-//   res.send("Welcome");
-// });
-// ​
-// server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
