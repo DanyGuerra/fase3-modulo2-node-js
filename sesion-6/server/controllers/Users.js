9a20e72b170bc0fa2f2260467e0bdaf64e4a71c8
DanyGuerra <luis_daniel.grra15@hotmail.com>
2021-11-25 14:01:06 -0600

    Agregando controladores y CRUD con sequelize

---
+++ sesion-6/server/controllers/Users.js
@@ -0,0 +1,60 @@
+const User = require("../db/models").User;
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.send({ users });
+  } catch (err) {}
+};
+
+const createUser = async (req, res) => {
+  try {
+    const body = req.body;
+    const user = await User.create(body);
+    res.status(201).send({ user });
+  } catch (err) {
+    res.status(400).send("Bad request");
+  }
+};
+
+const getUserById = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await User.findOne({ where: { id: id } });
+    res.send({ user });
+  } catch (err) {
+    res.status(400).send("Bad request");
+  }
+};
+
+const putUserById = async (req, res) => {
+  const id = req.params.id;
+  console.log(req.body.email);
+  console.log(id);
+  try {
+    await User.update({ email: req.body.email }, { where: { id: id } });
+    res.send("User updated");
+  } catch (err) {}
+};
+
+const deleteUserById = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await User.destroy({ where: { id: id } });
+    if (user) {
+      res.send("User deleted");
+    } else {
+      res.send(400).send("User Not Found");
+    }
+  } catch (err) {
+    res.status(400).send("Bad request");
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  createUser,
+  getUserById,
+  deleteUserById,
+  putUserById,
+};
--- sesion-6/server/routes/users.js
+++ sesion-6/server/routes/users.js
@@ -2,36 +2,23 @@ var express = require("express");
 var router = express.Router();
 const db = require("../db");
 const User = require("../db/models").User;
+const {
+  getAllUsers,
+  createUser,
+  getUserById,
+  deleteUserById,
+  putUserById,
+} = require("../controllers/Users");
 
 /* GET users listing. */
-router.get("/", async function (req, res) {
-  const users = await User.findAll();
-  res.send({ users });
-});
+router.get("/", getAllUsers);
 
-router.post("/", async function (req, res) {
-  const body = req.body;
-  const user = await User.create(body);
-  res.status(201).send({ user });
-});
+router.post("/", createUser);
 
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  const user = db.models.Users.findOne({ id });
-  console.log(user);
-  res.send(user);
-});
+router.get("/:id", getUserById);
 
-router.put("/:id", (req, res) => {
-  const id = req.params.id;
-  const user = db.models.Users.updateOne({ id }, req.body);
-  res.send(user);
-});
+router.put("/:id", putUserById);
 
-router.delete("/:id", (req, res) => {
-  const id = req.params.id;
-  const user = db.models.Users.remove({ id });
-  res.status(200).send(user);
-});
+router.delete("/:id", deleteUserById);
 
 module.exports = router;
